94612d4d46af1b363aa175fd38ebc237c78310bb
songb <songbaodu@163.com>
2019-11-21 23:20:58 +0800

    添加登录退出功能

---
+++ src/main/java/cn/itcast/travel/service/impl/UserServiceImpl.java
@@ -0,0 +1,61 @@
+package cn.itcast.travel.service.impl;
+
+import cn.itcast.travel.dao.UserDao;
+import cn.itcast.travel.dao.impl.UserDaoImpl;
+import cn.itcast.travel.domain.User;
+import cn.itcast.travel.service.UserService;
+import cn.itcast.travel.util.MailUtils;
+import cn.itcast.travel.util.UuidUtil;
+
+public class UserServiceImpl implements UserService {
+
+     private UserDao userDao = new UserDaoImpl();
+    /**
+     * 注册用户
+     * @param user
+     * @return
+     */
+
+
+    @Override
+    public boolean regist(User user) {
+        //1.根据用户名查询用户对象
+        User u = userDao.findByUsername(user.getUsername());
+        if (u != null){
+            return false;
+        }
+        //2.保存用户信息
+        user.setCode(UuidUtil.getUuid());
+        user.setStatus("N");
+        userDao.save(user);
+        //3.发送激活邮件
+        String content = "<a href='http://localhost:8080/travel/activeUserServlet?code="+user.getCode()+"'>点击激活</a>";
+        MailUtils.sendMail(user.getEmail(),content,"激活邮件");
+        return true;
+    }
+
+    /**
+     * 激活用户
+     * @param code
+     * @return
+     */
+    @Override
+    public boolean active(String code) {
+        //1.根据激活码查询用户对象
+        User user = userDao.findByCode(code);
+        if (user != null){
+            userDao.updateStatus(user);
+            return true;
+        }else {
+            return false;
+        }
+        //2.调用DAO修改激活状态
+
+
+    }
+
+    @Override
+    public User login(User user) {
+        return userDao.findByUsernameAndPassword(user.getUsername(),user.getPassword());
+    }
+}
---
+++ src/main/java/cn/itcast/travel/web/servlet/LoginServlet.java
@@ -0,0 +1,62 @@
+package cn.itcast.travel.web.servlet;
+
+import cn.itcast.travel.domain.ResultInfo;
+import cn.itcast.travel.domain.User;
+import cn.itcast.travel.service.UserService;
+import cn.itcast.travel.service.impl.UserServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+@WebServlet(value = "/loginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //1.获取用户名密码
+        Map<String, String[]> map = request.getParameterMap();
+        //2.封装User对象
+        User user = new User();
+        try {
+            BeanUtils.populate(user,map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        //3.调用Service查询
+        UserService service = new UserServiceImpl();
+        User u = service.login(user);
+
+        ResultInfo info = new ResultInfo();
+        //4.判断用户名密码
+        if (u == null){
+            //用户名密码错误
+            info.setFlag(false);
+            info.setErrorMsg("用户名或密码错误");
+        }
+        //5.是否激活
+        if (u != null && !"Y".equals(u.getStatus())){
+            info.setFlag(false);
+            info.setErrorMsg("您尚未激活,请激活");
+        }
+        if (u != null && "Y".equals(u.getStatus())){
+            request.getSession().setAttribute("user",u);//登录成功标记
+            info.setFlag(true);
+        }
+        //响应数据
+        ObjectMapper mapper = new ObjectMapper();
+        response.setContentType("application/json;charset=utf-8");
+        mapper.writeValue(response.getOutputStream(),info);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
